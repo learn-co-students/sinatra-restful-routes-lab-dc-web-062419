c157e1a8e40bd65474ad44ee3280ec77a16daab7
Michelle Lam <shelly2.lam@gmail.com>
2019-07-16 15:16:30 -0400

    Claudia and Michelle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  set :method_override, true
   # code actions here!
+
+  get '/' do
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/new" do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    # recipe = Recipe.update(@recipe.id, name: name, ingredients: ingredients, cook_time: cook_time)
+    redirect "/recipes/#{@recipe.id}"
+  end
 
 end
